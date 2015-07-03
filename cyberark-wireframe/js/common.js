80bafdf3e917b9c03b778567b6b1f306d38173d0
Yan White <yan.white@gmail.com>
2015-07-03 10:20:25 +0100

    first commit

--- cyberark-wireframe/js/common.js
+++ cyberark-wireframe/js/common.js
@@ -17,6 +17,6 @@ $(function () {
         scrollBar: true,
 //        anchors: ['page1', 'page2', 'page3', 'page4'],
         anchors: [ ['page1', 'page2'], ['page3', 'page4', 'page5'], ['page6', 'page7', 'page8', 'page9'], ['page10', 'page11', 'page12'], ['page13', 'page14', 'page15'] ],
-        // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+        // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'] 
     });
 });
