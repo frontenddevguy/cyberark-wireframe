8a7f21841a811fdd2c3c28e66e2c82bb93a86d9a
Rajat <rajatverma.365@gmail.com>
2015-07-06 18:27:53 +0530

    Changes Slider

--- app/scripts/lib/common.js
+++ app/scripts/lib/common.js
@@ -1,10 +1,35 @@
 $(function () {
 
+
     $('#pagepiling').fullpage({
         fitToSection: true, 'navigation': true,
         'navigationPosition': 'right',
         scrollBar: true,
-        anchors: [['page1', 'page2'], ['page3', 'page4', 'page5'], ['page6', 'page7','page8', 'page9'], ['page10', 'page11','page12'], ['page13', 'page14','page15'] ],
+        anchors: [['page1', 'page2'], ['page3', 'page4', 'page5'], ['page6', 'page7', 'page8', 'page9'], ['page10', 'page11', 'page12'], ['page13', 'page14', 'page15']],
         // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
     });
+
+
+    $('#myCarouselSec4').carousel({
+        interval: 40000
+    });
+
+    $('.carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+
+            next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+
+        }
+    });
+
 });
