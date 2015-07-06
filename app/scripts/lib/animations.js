2e0a8fd1b2f6174db4a70a73dd1fe7374aa9cefe
Yan White <yan.white@gmail.com>
2015-07-06 12:16:35 +0100

    renamed sectio in animations.jsn

--- app/scripts/lib/animations.js
+++ app/scripts/lib/animations.js
@@ -3,9 +3,9 @@ $(function () {
 
     for (var i = 1; i <= 15; i++) {
         new ScrollMagic.Scene({
-            triggerElement: '#scene-' + i,
+            triggerElement: '#section-' + i,
             offset: 20
-        }).setTween("#scene-" + i, 0.6, {backgroundColor: "transparent", scale: 1, alpha: 1, zIndex: 99})
+        }).setTween("#section-" + i, 0.6, {backgroundColor: "transparent", scale: 1, alpha: 1, zIndex: 99})
                 .addTo(scrollMagicController);
     }
 
@@ -19,40 +19,40 @@ $(function () {
         height: 186
     });
 
-    var p2 = TweenMax.to('.xsbottom-bg', 1, {
-        height: 250
-    });
+    // var p2 = TweenMax.to('.xsbottom-bg', 1, {
+    //     height: 250
+    // });
 
-    var p3 = TweenMax.to('#business_leadership', 0.5, {
-        height: 471
+    var p3 = TweenMax.to('#business_leadership', 1, {
+        height: 471, width: 300
     });
 
     var p4 = TweenMax.to('#business_leadership', 0.5, {
-        height: 450
+        height: 600
     });
 
     new ScrollMagic.Scene({
-        triggerElement: '#scene-1',
+        triggerElement: '#section-1',
         offset: 10
     }).setTween(p1)
             .addTo(scrollMagicController);
 
     new ScrollMagic.Scene({
-        triggerElement: '#scene-2',
+        triggerElement: '#section-2',
         offset: 10
     }).setTween(p2)
             .addTo(scrollMagicController);
 
 
     new ScrollMagic.Scene({
-        triggerElement: '#scene-3',
+        triggerElement: '#section-3',
         offset: 10
     }).setTween(p3)
             .addTo(scrollMagicController);
 
 
     new ScrollMagic.Scene({
-        triggerElement: '#scene-4',
+        triggerElement: '#section-4',
         offset: 10
     }).setTween(p4)
             .addTo(scrollMagicController);
