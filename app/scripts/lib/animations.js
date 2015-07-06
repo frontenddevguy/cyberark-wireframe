55421551dc80811ab27c4cbd7d33650d35f234f0
Yan White <yan.white@gmail.com>
2015-07-06 13:25:40 +0100

    test

--- app/scripts/lib/animations.js
+++ app/scripts/lib/animations.js
@@ -18,43 +18,49 @@ $(function () {
     var p1 = TweenMax.to('.xsbottom-bg', 1, {
         height: 186
     });
-
-    // var p2 = TweenMax.to('.xsbottom-bg', 1, {
-    //     height: 250
-    // });
-
-    var p3 = TweenMax.to('#business_leadership', 1, {
-        height: 471, width: 300
-    });
-
-    var p4 = TweenMax.to('#business_leadership', 0.5, {
-        height: 600
-    });
-
     new ScrollMagic.Scene({
         triggerElement: '#section1',
         offset: 10
     }).setTween(p1)
             .addTo(scrollMagicController);
 
-    new ScrollMagic.Scene({
-        triggerElement: '#section2',
-        offset: 10
-    }).setTween(p2)
-            .addTo(scrollMagicController);
 
+// trigger on section 3
+    var p3 = TweenMax.to('#business_leadership', 1, {
+        height: 471, width: 300
+    });
+    var p31 = TweenMax.to('#business_leadership2', 1, {
+        height: 300, width: 300
+    });
 
     new ScrollMagic.Scene({
         triggerElement: '#section3',
         offset: 10
     }).setTween(p3)
             .addTo(scrollMagicController);
+    new ScrollMagic.Scene({
+        triggerElement: '#section3',
+        offset: 10
+    }).setTween(p31)
+            .addTo(scrollMagicController);
 
+
+// trigger on section 3
+    var p4 = TweenMax.to('#business_leadership', 1, {
+        height: 0, width: 300
+    });
+    var p41 = TweenMax.to('#business_leadership2', 1, {
+        height: 0, width: 300
+    });
 
     new ScrollMagic.Scene({
         triggerElement: '#section4',
         offset: 10
     }).setTween(p4)
             .addTo(scrollMagicController);
-
+    new ScrollMagic.Scene({
+        triggerElement: '#section4',
+        offset: 10
+    }).setTween(p41)
+            .addTo(scrollMagicController);
 });
